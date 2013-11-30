0bf9eaab6d8499244beb01e147c9ad935e5ab5ef
Shunting <zhangst1986@gmail.com>
2013-11-30 17:54:18 -0500

    finish RSA encrypt/decrypt

--- src/crypto/RSA.scala
+++ src/crypto/RSA.scala
@@ -1,5 +1,6 @@
 package crypto
 
+import util._
 import util.Util._
 import cert._
 
@@ -8,30 +9,74 @@ object RSA {
 		// Do encryption first
 		val cert = new X509Certificate
 		cert.parsePemFile("/tmp/server.crt")
-		// TODO
 
-		/*
-		// steam to byte array
-		val input = streamToByteArray(System.in)
+		// plain text
+		val plainText = streamToByteArray(System.in)
+		printf("Origin Plain Text:\n")
+		dumpByteArray(plainText)
 
-		// byte array to big number
-		val bi = byteArrayToBigInt(input)
+		// do encryption
+		val rsa = new RSA
+		val genCipherText = rsa.encrypt(plainText, cert.publicKey)
 
-		// load private key
 		val privateKey = new PrivateKey
 		privateKey.parsePemFile("/tmp/server.key")
 
-		val rsa = new RSA
-		val res = rsa.transformToByteArray(bi, privateKey.privateExponent, privateKey.modulus)
-
-		dumpByteArray(res)
-		 */
+		val genPlainText = rsa.decrypt(genCipherText, privateKey)
+		printf("generated plain text:\n")
+		dumpByteArray(genPlainText)
 	}
 }
 
 class RSA {
-	def transformToByteArray(base: BigInt, exponent: BigInt, modulus: BigInt): Array[Byte] = {
-		val resBi = transform(base, exponent, modulus)
+	def decrypt(cipherText: Array[Byte], privateKey: PrivateKey): Array[Byte] = {
+		val cipherBi = byteArrayToBigInt(cipherText)
+		val privateExponent = privateKey.privateExponent
+		val modulus = privateKey.modulus
+
+		assert(cipherBi < modulus)
+		val resBi = transform(cipherBi, privateExponent, modulus)
+		removePadding(resBi, modulus)
+	}
+
+	def encrypt(plainText: Array[Byte], publicKey: PublicKey): Array[Byte] = {
+		val publicExponent = publicKey.publicExponent
+		val modulus = publicKey.modulus
+
+		val paddedPlainText = addPadding(plainText, publicKey.modulus)
+		val base = Util.byteArrayToBigInt(paddedPlainText)
+		val resBi = transform(base, publicExponent, modulus)
+		Util.bigIntToByteArray(resBi, (modulus.bitLength + 7) / 8)
+	}
+
+	def addPadding(plainText: Array[Byte], modulus: BigInt): Array[Byte] = {
+		// EB = 00 || BT || PS || 00 || D
+		val totLen = (modulus.bitLength + 7) / 8
+		assert(plainText.length + 3 <= totLen)
+
+		val blockType: Byte = 2
+
+		def genPadding(len: Int): Array[Byte] = {
+			if (len == 0) {
+				Array[Byte]()
+			} else {
+				def genOnePadding: Byte = {
+					val randAr = Util.genRandom(1)
+					if (randAr(0) == 0) {
+						genOnePadding
+					} else {
+						randAr(0)
+					}
+				}
+				genPadding(len - 1) :+ genOnePadding
+			}
+		}
+
+		var paddingStr = genPadding(totLen - 3 - plainText.length)
+		Array[Byte](0) ++ Array[Byte](blockType) ++ paddingStr ++ Array[Byte](0) ++ plainText
+	}
+
+	def removePadding(resBi: BigInt, modulus: BigInt): Array[Byte] = {
 		val mbitlen = modulus.bitLength
 		val mbytelen = (mbitlen + 7) / 8 
 		val resAr = bigIntToByteArray(resBi, mbytelen)
